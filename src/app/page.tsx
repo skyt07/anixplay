8f8bf0bc3251bb060750174a389a095ac2540e80
skyt07 <158485176+skyt07@users.noreply.github.com>
2026-05-13 21:34:42 +0530

    fixed the page sturcture

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+// import HeroCarousel from "@/components/HeroCarousel";
+
+import ShareSite from "@/components/ShareSite";
+
+export default function Home() {
+  return (
+    <>
+      {/* <HeroCarousel /> */}
+      <section>
+        <main>
+          <ShareSite />
+        </main>
+        <aside></aside>
+      </section>
+    </>
+  );
+}
