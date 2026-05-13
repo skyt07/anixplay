1e308d15b7801991f36152ad0284bc20509ccbd7
skyt07 <158485176+skyt07@users.noreply.github.com>
2026-05-14 00:07:02 +0530

    restored the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 // import HeroCarousel from "@/components/HeroCarousel";
-// import ShareSite from "@/components/ShareSite";
+import ShareSite from "@/components/ShareSite";
 
 export default function Home() {
   return (
     <>
       <h1>hello Anixplay</h1>
-      {/* <HeroCarousel /> */}
-      {/* <section>
+      {/* <HeroCarousel />  */}
+      <section>
         <main>
           <ShareSite />
         </main>
         <aside></aside>
-      </section> */}
+      </section>
     </>
   );
 }
