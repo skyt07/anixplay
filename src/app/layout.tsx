7ce295cb40759928e8ced0150f791a5bb8983b6c
skyt07 <158485176+skyt07@users.noreply.github.com>
2026-05-16 23:09:26 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "../styles/globals.css";
 import "../styles/animate.css";
 import "../styles/utils.css";
@@ -43,6 +44,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <Header />
         {children}
         <Footer />
